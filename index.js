e976b85db8b7b81cbf4a5e1419af542f607cc810
eddielicaycay <eddielicaycay@gmail.com>
2023-12-12 14:04:34 -0800

    polishing web

--- index.js
+++ index.js
@@ -22,41 +22,12 @@ button.addEventListener('click', (x) => {
     myLi.remove()
   });
   myLi.appendChild(mySpan);
+
+  // else, if nothing is placed in the box, return Invalid input
   } else {
     alert("Invalid input. Please enter a task!")
   }
+  // set value of inputText to nothing ""
   inputText.value = "";
 });
-
-
-// button.addEventListener('click', addItem);
-// inputText.addEventListener('keypress', (x) => {
-//   if(x.key === 'Enter') {
-//     addItem(x);
-//   }
-// });
-
-// const addItem('click', (x) => {
-//   if(inputText.value !== "") {
-//     x.preventDefault();
-
-//     // Create li where input value is added to the list
-//     const myLi = document.CreateElement('li');
-//     myLi.innerHTML = inputText.value;
-//     list.appendChild(myLi);
-
-//     // Create a span where the 'x' can be used to delete a list
-//     const mySpan = document.CreateElement('span')
-//     mySpan.innerHTML = "x"
-
-//     // Add a click event listener to the span 'X' to remove listed item
-//     mySpan.addEventListener('click', () => {
-//       myLi.remove();
-//     })
-//     myLi.appendChild(mySpan);
-//   }
-//   inputText.value = "";
-// }
-
-
 
